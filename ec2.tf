a3c25a059655242c98557b3fda59126bd0f04cc7
aalroy <98309142+aalroy@users.noreply.github.com>
2022-10-26 08:48:30 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -163,7 +163,7 @@ resource "aws_security_group" "web-node" {
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "b7af1b40-64eb-4519-a1a0-ab198db4b193"
-    component             = "A1111"
+    component             = "A22"
   }
 }
 
